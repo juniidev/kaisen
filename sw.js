d57cbf942fb9b64cc028aeab67002373484c058a
Henry <90808009+juniidev@users.noreply.github.com>
2026-05-19 11:24:02 -0600

    Add first files

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'fase2-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://jsdelivr.net'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
